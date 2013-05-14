de28b170c98dd0354a6764e2464a0db82358aae6
LiYang <bbtfrr@gmail.com>
2013-05-14 17:58:15 +0800

    remove column choices

--- db/migrate/5_create_style_clicks.rb
+++ db/migrate/5_create_style_clicks.rb
@@ -2,7 +2,6 @@ class CreateStyleClicks < ActiveRecord::Migration
   def change
     create_table :refinery_style_clicks do |t|
       t.belongs_to :game
-      t.string :choices
       t.belongs_to :result
       t.string :remote_ip
       t.string :agent
